04a2720957d1b97da2a1fcf43af20d45e8d32a3a
Shashwat Sharma <shashwats09@gmail.com>
2021-02-02 16:26:52 +0530

    tweaked text length criterion and refactored occupation filters
    - checks text length to be > 1700
    - takes p tags which are direct children of mw-parser-output
    - occupation and keywords are now stored in a dictionary
    - using tqdm.write() in place of print() to avoid repeating progress bar

--- basic_scraper.py
+++ basic_scraper.py
@@ -26,6 +26,12 @@ class Person(StructuredNode):
     friends = RelationshipTo("Person", "KNOWS")
     politician = BooleanProperty()
 
+# stores occupation and corresponding keywords
+occupation_filters = {
+    "politician": ['politician'],
+    "cricketer": ['cricket'],
+}
+
 NAME = "L. K. Advani"
 q.put(NAME)
 cnt = 0
@@ -35,20 +41,18 @@ while cnt < 50:
     page = requests.get(URL)
 
     soup = BeautifulSoup(page.content, "html.parser")
-    # print(soup.get_text())
     dict1 = soup.find_all("table", class_="infobox")
     s = dict1[0].get_text()
-    # print(s)
     index = s.find("Born")
     curr = ""
     if index > 0:
         print("PERSON")
-        try:
-            curr = Person(name=NAME).save()
-            cnt = cnt + 1
-        except:
-            curr = Person.nodes.get(name=NAME)
-        # curr = Person.get_or_create(name='Saurav Ganguly').save()
+        cnt += 1
+        # try:
+        #     curr = Person(name=NAME).save()
+        #     cnt = cnt + 1
+        # except:
+        #     curr = Person.nodes.get(name=NAME)
     else:
         print("NOT PERSON")
 
@@ -59,66 +63,77 @@ while cnt < 50:
 
     for link in tqdm(url_dict):
         url2 = "https://en.wikipedia.org/" + str(link.get("href"))
-
         ss = str(link.get("title"))
         x = ss.split(" ")
         if len(x) > 3:
             continue
-
+        
         page2 = requests.get(url2)
         soup2 = BeautifulSoup(page2.content, "html.parser")
 
+        #Born check
         dict2 = soup2.find_all("table", class_="infobox")
-
         if len(dict2) == 0:
             continue
-
         s = dict2[0].get_text()
-        # print(s)
         index = s.find("Born")
-        if index > 0:
-            print(link.get("title"))
-            print("PERSON")
-            text_dict = soup2.findAll("p")
-            c = 0
-            for para in text_dict:
-                ss = para.get_text()
-                sss = ss.split(" ")
-                c += len(sss)
-            print(c)
-            if c < 2500:
-                continue
-            first_para=text_dict[1].get_text()
-            first_para_dict=first_para.split(".")
-            line=first_para_dict[0]
-            line = soup2.select("table~p")[0].getText()
-            line = line.split(".")[0]
-            print(f'line is {line}')
-            index1=line.find("Indian")
-            if index1<0 :
-                print("NOT INDIAN")
-                continue
+        if index < 0:
+            continue
+        tqdm.write("")
+        tqdm.write(link.get("title"))
+        tqdm.write("Found Born")
 
-            now = Person(name=link.get("title"))
+        #Text length check
+        text_dict = soup2.select(".mw-parser-output > p")
+        c = 0
+        for para in text_dict:
+            ss = para.get_text()
+            sss = ss.split(" ")
+            c += len(sss)
+        if c < 1700:
+            continue
+        tqdm.write(f'Text length is {c}')
 
-            index2=line.find("cricket")
-            if index2>0:
-                print("Cricketer")
-            index3=line.find("politician")
-            if index3>0:
-                print("Politician")
-                now.politician = True
-            else:
-                now.politician = False
-            try:
-                now.save()
-                print(f'Saved {link.get("title")}')
-                q.put(link.get("title"))
-            except:
-                now = Person.nodes.get(name=link.get("title"))
-                print(f'Not saved {link.get("title")}', sys.exc_info()[0])
-            finally:
-                curr.friends.connect(now)
+        #Find the first paragraph of content
+        first_para = ''
+        for el in text_dict:
+            first_para = el.getText(strip=True)
+            if len(first_para) > 0:
+                break
+
+        # line = first_para.split('.')[0]
+        line = first_para.split('\n')[0]
+        tqdm.write(f'Line is : {line}')
+
+        #Indian check
+        index1=line.find("Indian")
+        if index1 < 0 :
+            tqdm.write("NOT INDIAN")
+            continue
+
+        now = Person(name=link.get("title"))
+        
+        # Adding occupation information
+        for occupation, keywords in occupation_filters.items():
+            setattr(now, occupation, False)
+            for keyword in keywords:
+                if(line.find(keyword) > 0):
+                    tqdm.write(occupation)
+                    setattr(now, occupation, True)
+                    break
+
+        q.put(link.get("title"))
+        
+        # Saving into database
+        # try:
+        #     now.save()
+        #     print(f'Saved {link.get("title")}')
+            # q.put(link.get("title"))
+        # except:
+        #     now = Person.nodes.get(name=link.get("title"))
+        #     print(f'Not saved {link.get("title")}', sys.exc_info()[0])
+        # finally:
+        #     curr.friends.connect(now)
 
     print("Done")
     # final_dict[curr_person]=str(link.get('title'))
